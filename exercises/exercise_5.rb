c016006b563540a1ede37bf9cb5db4b24b894da8
Iso <kagiso.k.mashigo@gmial.com>
2021-01-05 14:09:51 +0000

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@all_stores = Store.sum(:annual_revenue)
+@ave_stores = Store.average(:annual_revenue)
+@big_stores = Store.where("annual_revenue < 1000000").size
+
+pp "Total company revenue: #{@all_stores} with average of #{@ave_stores}"
+
+pp "There are #{@big_stores} stores making over a million in revenue"
